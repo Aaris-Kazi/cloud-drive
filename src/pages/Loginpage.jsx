f2e880123363d3fe2482670e71680a68e4ac054d
Aaris-Kazi <aariskazi@gmail.com>
2025-02-09 18:26:08 +0530

    UPDATE: authentication complete

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -2,82 +2,115 @@ import { useLocation } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import googledrive from "../assets/googledrive.svg";
+import { useNavigate } from "react-router-dom";
 import "./css/LoginPage.css";
+import { create } from "../utils/webclient";
 import ErrorComponent from "../components/ErrorComponent";
 
 const Loginpage = () => {
-  const location = useLocation();
-  const [email, setEmail] = useState("");
+    const location = useLocation();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
-  useEffect(() => {
-    if (location.state?.email) {
-      setEmail(location.state.email);
-    }
-  }, [location.state]);
+    const navigate = useNavigate();
 
-  return (
-    <div className="container loginPage">
-      <div className="row">
-        <div className="col">
-          <div className="container login">
+    useEffect(() => {
+        if (location.state?.email) {
+            setEmail(location.state.email);
+        }
+    }, [location.state]);
+
+    const submittingForm = async (e) => {
+        /*
+            Controller method to control route
+        */
+        e.preventDefault();
+        try {
+            const res = await create("/auth/login/", {
+                email: email,
+                password: password
+            });
+
+            if (res.status === 200) {
+                localStorage.setItem("cloud_drive_username", res.data.username);
+                localStorage.setItem("cloud_drive_access_token", res.data.access_token);
+                navigate("/home");
+            } else {
+                setError(res.data);
+            }
+        } catch (exception) {
+            console.log(exception);
+            setError(exception);
+        }
+    };
+
+    return (
+        <div className="container loginPage">
             <div className="row">
-              <div className="col login">
-                <nav className="navbar navbar-expand-lg">
-                  <div className="container-fluid navbar">
-                    <Link to={"/"} className="navbar-brand">
-                      <img
-                        src={googledrive}
-                        alt="Bootstrap"
-                        width="30"
-                        height="24"
-                      />
-                      Cloud Drive
-                    </Link>
-                  </div>
-                </nav>
-                <div className="row">
-                  <div className="row">
-                    <span className="h4">Sign in</span>
-                  </div>
-                </div>
-                <div className="row">
-                  <ErrorComponent message={"Invalid username or password"} />
+                <div className="col">
+                    <div className="container login">
+                        <div className="row">
+                            <div className="col login">
+                                <nav className="navbar navbar-expand-lg">
+                                    <div className="container-fluid navbar">
+                                        <Link to={"/"} className="navbar-brand">
+                                            <img
+                                                src={googledrive}
+                                                alt="Bootstrap"
+                                                width="30"
+                                                height="24"
+                                            />
+                                            Cloud Drive
+                                        </Link>
+                                    </div>
+                                </nav>
+                                <div className="row">
+                                    <div className="row">
+                                        <span className="h4">Sign in</span>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    {error !== "" && (
+                                        <ErrorComponent message={"Invalid username or password"} />
+                                    )}
 
-                  <form action="" method="post">
-                    <input
-                      className="form-control login-form"
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="someone@example.com"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.email)}
-                    />
-                    <input
-                      className="form-control login-form"
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="password"
-                    />
-                    <div className="secondary-content">
-                      No account? <Link to={"/register"}>Create one!</Link>
+                                    <form onSubmit={submittingForm}>
+                                        <input
+                                            className="form-control login-form"
+                                            type="email"
+                                            name="email"
+                                            id="email"
+                                            placeholder="someone@example.com"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                        />
+                                        <input
+                                            className="form-control login-form"
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            placeholder="password"
+                                            onChange={(e) => setPassword(e.target.value)}
+                                        />
+                                        <div className="secondary-content">
+                                            No account? <Link to={"/register"}>Create one!</Link>
+                                        </div>
+                                        <button
+                                            type="submit"
+                                            className="btn btn-primary login-form"
+                                        >
+                                            Next
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <button
-                      type="submit"
-                      className="btn btn-primary login-form"
-                    >
-                      Next
-                    </button>
-                  </form>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Loginpage;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -3,132 +3,131 @@ import { Link } from "react-router-dom";
 import googledrive from "../assets/googledrive.svg";
 import { useNavigate } from "react-router-dom";
 import "./css/LoginPage.css";
-// import create from "../utils/webclient";
 import { create } from "../utils/webclient";
 import ErrorComponent from "../components/ErrorComponent";
 
 const RegisterPage = () => {
-  const [username, setUsername] = useState("test21");
-  const [firstname, setFirstname] = useState("aaris");
-  const [lastname, setLastname] = useState("kazi");
-  const [email, setEmail] = useState("test21@gmail.com");
-  const [password, setPassword] = useState("12345");
-  const [error, setError] = useState("");
+    const [username, setUsername] = useState("");
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const submittingForm = async (e) => {
-    /*
-        Controller method to control route
-        */
-    e.preventDefault();
-    try {
-      const res = await create("/auth/register/", {
-        username: username,
-        first_name: firstname,
-        last_name: lastname,
-        email: email,
-        password: password,
-      });
+    const submittingForm = async (e) => {
+        /*
+            Controller method to control route
+            */
+        e.preventDefault();
+        try {
+            const res = await create("/auth/register/", {
+                username: username,
+                first_name: firstname,
+                last_name: lastname,
+                email: email,
+                password: password
+            });
 
-      if (res.status === 200) {
-        navigate("/login", { state: { email } });
-      } else {
-        setError(res.data);
-      }
-    } catch (exception) {
-      console.log(exception);
-      setError(exception);
-    }
-  };
+            if (res.status === 200) {
+                navigate("/login", { state: { email } });
+            } else {
+                setError(res.data);
+            }
+        } catch (exception) {
+            console.log(exception);
+            setError(exception);
+        }
+    };
 
-  return (
-    <div className="container loginPage">
-      <div className="row">
-        <div className="col">
-          <div className="container login">
+    return (
+        <div className="container loginPage">
             <div className="row">
-              <div className="col login">
-                <nav className="navbar navbar-expand-lg">
-                  <div className="container-fluid navbar">
-                    <Link to={"/"} className="navbar-brand">
-                      <img
-                        src={googledrive}
-                        alt="Bootstrap"
-                        width="30"
-                        height="24"
-                      />
-                      Cloud Drive
-                    </Link>
-                  </div>
-                </nav>
-                <div className="row">
-                  <div className="row">
-                    <span className="h4">Create account</span>
-                  </div>
-                </div>
-                <div className="row">
-                    { error !== "" && (
+                <div className="col">
+                    <div className="container login">
+                        <div className="row">
+                            <div className="col login">
+                                <nav className="navbar navbar-expand-lg">
+                                    <div className="container-fluid navbar">
+                                        <Link to={"/"} className="navbar-brand">
+                                            <img
+                                                src={googledrive}
+                                                alt="Bootstrap"
+                                                width="30"
+                                                height="24"
+                                            />
+                                            Cloud Drive
+                                        </Link>
+                                    </div>
+                                </nav>
+                                <div className="row">
+                                    <div className="row">
+                                        <span className="h4">Create account</span>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    {error !== "" && (
 
-                    <ErrorComponent message={"User already exist"} />
-                    )}
-                  
-                  <form onSubmit={submittingForm}>
-                    <input
-                      className="form-control login-form"
-                      type="text"
-                      name="username"
-                      id="username"
-                      placeholder="username"
-                      onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <input
-                      className="form-control login-form"
-                      type="text"
-                      name="firstname"
-                      id="firstname"
-                      placeholder="first name"
-                      onChange={(e) => setFirstname(e.target.value)}
-                    />
-                    <input
-                      className="form-control login-form"
-                      type="text"
-                      name="lastname"
-                      id="lastname"
-                      placeholder="last name"
-                      onChange={(e) => setLastname(e.target.value)}
-                    />
-                    <input
-                      className="form-control login-form"
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="someone@example.com"
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                      className="form-control login-form"
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="password"
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button
-                      type="submit"
-                      className="btn btn-primary login-form"
-                    >
-                      Next
-                    </button>
-                  </form>
+                                        <ErrorComponent message={"User already exist"} />
+                                    )}
+
+                                    <form onSubmit={submittingForm}>
+                                        <input
+                                            className="form-control login-form"
+                                            type="text"
+                                            name="username"
+                                            id="username"
+                                            placeholder="username"
+                                            onChange={(e) => setUsername(e.target.value)}
+                                        />
+                                        <input
+                                            className="form-control login-form"
+                                            type="text"
+                                            name="firstname"
+                                            id="firstname"
+                                            placeholder="first name"
+                                            onChange={(e) => setFirstname(e.target.value)}
+                                        />
+                                        <input
+                                            className="form-control login-form"
+                                            type="text"
+                                            name="lastname"
+                                            id="lastname"
+                                            placeholder="last name"
+                                            onChange={(e) => setLastname(e.target.value)}
+                                        />
+                                        <input
+                                            className="form-control login-form"
+                                            type="email"
+                                            name="email"
+                                            id="email"
+                                            placeholder="someone@example.com"
+                                            onChange={(e) => setEmail(e.target.value)}
+                                        />
+                                        <input
+                                            className="form-control login-form"
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            placeholder="password"
+                                            onChange={(e) => setPassword(e.target.value)}
+                                        />
+                                        <button
+                                            type="submit"
+                                            className="btn btn-primary login-form"
+                                        >
+                                            Next
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default RegisterPage;
