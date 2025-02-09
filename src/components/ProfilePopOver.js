7d1a6dbf750349efb589347b97896c595b6fe154
Aaris-Kazi <aariskazi@gmail.com>
2025-02-09 23:04:25 +0530

    UPDATE: Logout function

--- src/components/ProfilePopOver.js
+++ src/components/ProfilePopOver.js
@@ -1,10 +1,24 @@
 import { RiImageCircleAiFill } from "react-icons/ri";
 import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 import '../pages/css/Popover.css'
+
+
+
+
 function ProfilePopOver() {
-
-
+    const username = localStorage.getItem("cloud_drive_username");
+    const email = localStorage.getItem("cloud_drive_email");
+    
+    const navigate = useNavigate();
+    
+    const handleLogout = () => {
+        localStorage.removeItem("cloud_drive_username");
+        localStorage.removeItem("cloud_drive_email");
+        localStorage.removeItem("cloud_drive_access_token"); // Remove token
+        navigate("/"); // Redirect to home page
+    };
 
     return (
         <div className='card text-end popup'  >
@@ -13,13 +27,13 @@ function ProfilePopOver() {
                 <div className='row'>
                     <div className='col text-start'><RiImageCircleAiFill className='image-icon' /></div>
                     <div className='col'>
-                        <div className='row'><h6>Aaris Kazi</h6></div>
-                        <div className='row'>aariskazi@gmail.com</div>
+                        <div className='row'><h6>{username}</h6></div>
+                        <div className='row'>{email}</div>
                         <div className='row'><Link to={''}>My profile</Link></div>
                     </div>
                 </div>
                 <div className='dash'></div>
-                <Link to={''} className='btn btn-primary'>Logout</Link>
+                <button className='btn btn-primary' onClick={handleLogout}>Logout</button>
             </div>
         </div>
 
