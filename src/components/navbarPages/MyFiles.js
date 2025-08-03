1fa4b0cc08796f2492ce4df59c9a4f1ac9375134
Aaris-Kazi <aariskazi@gmail.com>
2025-08-03 18:39:23 +0530

    UPDATE: file and folder

--- src/components/navbarPages/MyFiles.js
+++ src/components/navbarPages/MyFiles.js
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from "react";
 import Loader from "../Loader";
-import { FcFile } from "react-icons/fc";
+import { FcFile, FcFolder } from "react-icons/fc";
 import { fetchParams } from "../../utils/webclient";
 
 const MyFiles = () => {
     const [loader, setLoader] = useState(false);
-    const [dataDirectory, setdataDirectory] = useState([]);
+    const [dataDirectory, setdataDirectory] = useState({});
+    const [dataFiles, setdataFiles] = useState([]);
+    const [dataFolder, setdataFolder] = useState([]);
 
-    const access_token = localStorage.getItem("cloud_drive_access_token");
-
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + access_token
-    };
     useEffect(() => {
         const getFiles = async () => {
+            const access_token = localStorage.getItem("cloud_drive_access_token");
+
+            const headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + access_token
+            };
             setLoader(true);
             try {
                 const res = await fetchParams("/api/v1/directory/", headers);
                 if (res.status === 200) {
-                    setdataDirectory(res.data['direcotries'])
-                    console.log(res.data['direcotries']);
+                    const dirs = res.data.direcotries;
+                    setdataDirectory(dirs)
+
+                    if (dirs.file) {
+                        setdataFiles(dirs.file);
+                    } else {
+                        setdataFiles([]);
+                    }
+
+                    if (dirs.folder) {
+                        setdataFolder(dirs.folder);
+                    } else {
+                        setdataFolder([]);
+                    }
                 } else {
                     console.log(res.message);
                     console.log(res.status);
@@ -37,36 +51,49 @@ const MyFiles = () => {
     return (
         <div className="row">
             <span className='h4 button-title'>Files</span>
-            <table className='table'>
-                <thead>
-                    <tr>
-                        <th scope='col'>Name</th>
-                        <th scope='col-2'>Opened</th>
-                        <th scope='col-1'>Owner</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {loader && (
-                        <div className="row loader">
-                            <Loader />
-                        </div>
-                    )}
-                    {!loader && dataDirectory.map((file, index) => (
+            {loader && (
+                <div className="row loader">
+                    <Loader />
+                </div>
+            )}
+            {!loader && (
+                <table className='table'>
+                    <thead>
                         <tr>
-                            <td className='table-text'><FcFile className='margin-right-desktop' /> {file}</td>
-                            <td>5 Feb 2020</td>
-                            <td>Aaris Kazi</td>
+                            <th scope='col'>Name</th>
+                            <th scope='col-2'>Opened</th>
+                            <th scope='col-1'>Owner</th>
                         </tr>
-                    ))}
+                    </thead>
+                    <tbody>
+                        {Object.keys(dataDirectory).length === 0 && (
+                            <tr><td colSpan="3">No files found.</td></tr>
+                        )}
 
-                    {!loader && dataDirectory.length === 0 && (
-                        <tr><td colSpan="3">No files found.</td></tr>
-                    )}
+                        {!loader && dataFolder.map(
+                            (folder, index) => (
+                                <tr>
+                                    <td className='table-text'><FcFolder className='margin-right-desktop' /> {folder}</td>
+                                    <td>5 Feb 2020</td>
+                                    <td>Aaris Kazi</td>
+                                </tr>
+                            ))
+                        }
+
+                        {!loader && dataFiles.map((files, index) => (
+                            <tr>
+                                <td className='table-text'><FcFile className='margin-right-desktop' /> {files}</td>
+                                <td>5 Feb 2020</td>
+                                <td>Aaris Kazi</td>
+                            </tr>
+                        ))}
+                    </tbody>
 
 
-                </tbody>
 
-            </table>
+                </table>
+            )}
+
         </div>
     )
 }
